bd9351f01fcd63b1876c78a3164f2b291c6e703d
Pieux Xi <pieuxxi@gmail.com>
2012-06-22 10:26:55 +0800

    add xheditor except upload

---
+++ src/WebUI/Areas/Admin/Controllers/NewsAdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    public class NewsAdminController : Controller
+    {
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+    }
+}
