8758aa43c5331ad3b827c30a7d98a831d585c03c
Pieux Xi <pieuxxi@gmail.com>
2012-06-22 19:00:53 +0800

    autofac with parameter

--- src/EFData/RepositoriesModule.cs
+++ src/EFData/RepositoriesModule.cs
@@ -13,12 +13,19 @@ namespace EFData
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<FstClassRepository>().As<IFstClassRepository>().InstancePerDependency();
-            builder.RegisterType<SndClassRepository>().As<ISndClassRepository>().InstancePerDependency();
-            builder.RegisterType<NewsRepository>().As<INewsRepository>().InstancePerDependency();
+            PHeartDbContext context=new PHeartDbContext();
 
-            builder.RegisterType<UserRepository>().As<IUserRepository>().InstancePerDependency();
-            builder.RegisterType<RoleRepository>().As<IRoleRepository>().InstancePerDependency();
+            //builder.RegisterType<FstClassRepository>().As<IFstClassRepository>().InstancePerDependency();
+            //builder.RegisterType<SndClassRepository>().As<ISndClassRepository>().InstancePerDependency();
+            //builder.RegisterType<NewsRepository>().As<INewsRepository>().InstancePerDependency();
+            builder.Register(_ => new FstClassRepository(context)).As<IFstClassRepository>().InstancePerDependency();
+            builder.Register(_ => new SndClassRepository(context)).As<ISndClassRepository>().InstancePerDependency();
+            builder.Register(_ => new NewsRepository(context)).As<INewsRepository>().InstancePerDependency();
+
+            //builder.RegisterType<UserRepository>().As<IUserRepository>().InstancePerDependency();
+            //builder.RegisterType<RoleRepository>().As<IRoleRepository>().InstancePerDependency();
+            builder.Register(_ => new UserRepository(context)).As<IUserRepository>().InstancePerDependency();
+            builder.Register(_ => new RoleRepository(context)).As<IRoleRepository>().InstancePerDependency();
 
         }
     }
